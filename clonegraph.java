b7a9659effc05029f5501799ed274882cd8f8b1e
Aditya Kumar Manav <adityakumarmanav909@gmail.com>
2025-08-31 00:11:49 +0530

    Create clonegraph.java

---
+++ clonegraph.java
@@ -0,0 +1,18 @@
+class Solution {
+    private Map<Node, Node> visited = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+        if (visited.containsKey(node)) {
+            return visited.get(node);
+        }
+        Node clone = new Node(node.val);
+        visited.put(node, clone);
+        for (Node e : node.neighbors) {
+            clone.neighbors.add(cloneGraph(e));
+        }
+        return clone;
+    }
+}
